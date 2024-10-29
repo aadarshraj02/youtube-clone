69d6d7611237b5064ceb040ba35fcab03045047e
adarsh02 <aadarshraazz007@gmail.com>
2024-10-29 14:54:21 +0530

    added function to login user and create jwt token

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -31,3 +31,34 @@ export const registeredUser = async (req, res) => {
     });
   }
 };
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user)
+      return res.status(400).json({
+        message: "User not found please register",
+      });
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid)
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.json({
+      token,
+      userId: user._id,
+      username: user.username,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Login Failed",
+      error,
+    });
+  }
+};
