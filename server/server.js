96a846280839dda176a445bb5a7ad8e474cddb65
adarsh02 <aadarshraazz007@gmail.com>
2024-10-29 21:44:45 +0530

    added video routes

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/dbConnect.js";
 import authRoutes from "./routes/authRoutes.js";
+import videoRoutes from "./routes/videoRoutes.js";
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ connectDB();
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
+app.use("/api/videos", videoRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
