5b0f934b996df98621c933f737b5675b89ff1359
adarsh02 <aadarshraazz007@gmail.com>
2024-10-29 22:52:46 +0530

    added function to update channel channel name dec and banner

--- server/controllers/channelController.js
+++ server/controllers/channelController.js
@@ -37,3 +37,31 @@ export const getChannel = async (req, res) => {
     });
   }
 };
+
+export const updateChannel = async (req, res) => {
+  try {
+    const { channelId } = req.params;
+    const { channelName, description, channelBanner } = req.body;
+
+    const channel = await Channel.findById(channelId);
+
+    if (!channel) return res.status(404).json({ message: "Channel not found" });
+
+    if (channel.owner.toString() !== req.user._id.toString())
+      return res
+        .status(403)
+        .json({ message: "Not authorized to update this channel" });
+
+    channel.channelName = channelName || channel.channelName;
+    channel.description = description || channel.description;
+    channel.channelBanner = channelBanner || channel.channelBanner;
+
+    const updatedChannel = await channel.save();
+    res.json(updatedChannel);
+  } catch (error) {
+    return res.status(500).json({
+      message: "Channel not found",
+      error,
+    });
+  }
+};
