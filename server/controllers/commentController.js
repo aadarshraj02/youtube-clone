c46313041ff2ede569e68bdf0b174ad5bc439c55
adarsh02 <aadarshraazz007@gmail.com>
2024-10-31 02:45:55 +0530

    updated function to add comment

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -1,32 +1,28 @@
 import Video from "../model/Video";
 
+import Video from "../model/Video.js";
+
 export const addComment = async (req, res) => {
-  const { videoId } = req.params;
-  const { commentText } = req.body;
-
-  if (!req.user) {
-    return res
-      .status(401)
-      .json({ message: "You need to be signed in to comment." });
-  }
-
   try {
+    const { videoId } = req.params;
+    const { commentText } = req.body;
+    const userId = req.user.id;
+    const username = req.user.username;
+
     const video = await Video.findById(videoId);
-    if (!video) {
-      return res.status(404).json({ message: "Video not found" });
-    }
+    if (!video) return res.status(404).json({ message: "Video not found" });
+
     const newComment = {
-      userId: req.user.id,
-      username: req.user.username,
+      userId,
+      username,
       commentText,
+      timestamp: new Date(),
     };
     video.comments.push(newComment);
     await video.save();
 
-    res
-      .status(201)
-      .json({ message: "Comment added successfully", comment: newComment });
+    res.status(201).json({ message: "Comment added successfully", newComment });
   } catch (error) {
-    res.status(500).json({ message: "Error adding comment", error });
+    res.status(500).json({ message: "Unable to add comment", error });
   }
 };
