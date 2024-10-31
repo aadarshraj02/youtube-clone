25ca58c4db4f992e215b253c510186c7454ebfe2
adarsh02 <aadarshraazz007@gmail.com>
2024-10-31 16:00:50 +0530

    added function to delete comments

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -65,3 +65,32 @@ export const updateComment = async (req, res) => {
     });
   }
 };
+
+export const deleteComment = async (req, res) => {
+  const { videoId, commentId } = req.params;
+
+  try {
+    const video = await Video.findById(videoId);
+    if (!video)
+      return res.status(404).json({
+        message: "Video not found",
+      });
+
+    const comment = video.comments.id(commentId);
+    if (!comment)
+      return res.status(404).json({
+        message: "Comment not found",
+      });
+    if (comment.userId.toString() !== req.user.id) {
+      return res
+        .status(403)
+        .json({ message: "Unauthorized to delete this comment" });
+    }
+    comment.remove();
+    await video.save();
+
+    res.status(200).json({ message: "Comment deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Unable to delete comment", error });
+  }
+};
