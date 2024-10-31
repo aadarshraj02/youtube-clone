b37456efce659ef14167789d0ce6267ada173d49
adarsh02 <aadarshraazz007@gmail.com>
2024-10-31 17:10:48 +0530

    added sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,11 +1,41 @@
 import { RxHamburgerMenu } from "react-icons/rx";
+import { MdHome } from "react-icons/md";
+import { MdSubscriptions } from "react-icons/md";
+import { FaHistory } from "react-icons/fa";
+import { MdPlaylistPlay } from "react-icons/md";
 
 const Sidebar = (): JSX.Element => {
+  const menuItems = [
+    {
+      name: "Home",
+      icon: <MdHome />,
+    },
+    {
+      name: "Subscriptions",
+      icon: <MdSubscriptions />,
+    },
+    {
+      name: "History",
+      icon: <FaHistory />,
+    },
+    {
+      name: "Playlist",
+      icon: <MdPlaylistPlay />,
+    },
+  ];
   return (
-    <div>
+    <div className="px-4 bg-red-500 h-screen w-48">
       <div className="flex items-center">
         <RxHamburgerMenu />
         <img src="./youtubelogo.jpg" alt="youtube-logo" className="h-14" />
+      </div>
+      <div className="flex flex-col">
+        {menuItems.map((item, index) => (
+          <div key={index} className="flex items-center gap-4 mb-3">
+            {item.icon }
+            <h3 className="text-lg">{item.name}</h3>
+          </div>
+        ))}
       </div>
     </div>
   );
