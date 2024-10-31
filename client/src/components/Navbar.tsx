c172f42b5aee8f1d508a9a43edd25eb0dbadeaa7
adarsh02 <aadarshraazz007@gmail.com>
2024-10-31 16:36:00 +0530

    added functional componnet formatted code

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,14 +5,16 @@ const Navbar = (): JSX.Element => {
   return (
     <nav className="flex justify-between items-center px-4 py-2">
       <div className="flex items-center">
-      <RxHamburgerMenu/>
+        <RxHamburgerMenu />
 
-      <img src="./youtubelogo.jpg" alt="youtube-logo" className="h-14"  />
+        <img src="./youtubelogo.jpg" alt="youtube-logo" className="h-14" />
       </div>
       <div className="flex items-center gap-2">
-
-      <input className="border rounded-full px-3 py-1 w-[25vw]" placeholder="search videos here" />
-      <IoSearchOutline className="text-zinc-500" size={20} />
+        <input
+          className="border rounded-full px-3 py-1 w-[25vw]"
+          placeholder="search videos here"
+        />
+        <IoSearchOutline className="text-zinc-500" size={20} />
       </div>
       <button>Profile</button>
     </nav>
