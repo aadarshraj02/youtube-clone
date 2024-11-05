0d1bbde257e89630388dd6c606d3cf7d05cefd27
adarsh02 <aadarshraazz007@gmail.com>
2024-11-05 17:40:51 +0530

    added categoryFilter.tsx

--- client/src/components/CategoryFilter.tsx
+++ client/src/components/CategoryFilter.tsx
@@ -1,5 +1,40 @@
-const CategoryFilter = (): JSX.Element => {
-  return <div>CategoryFilter</div>;
+import { useState } from "react";
+
+const Category = (): JSX.Element => {
+  const categories = [
+    "All",
+    "Gaming",
+    "Study",
+    "Anime",
+    "Funny",
+    "Tech",
+    "Sports",
+    "Travel",
+  ];
+
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const handleCategorySelect = (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  return (
+    <div className="flex gap-4 overflow-x-auto px-4 py-2 whitespace-nowrap">
+      {categories.map((category) => (
+        <button
+          key={category}
+          onClick={() => handleCategorySelect(category)}
+          className={`px-6 py-1 rounded-lg ${
+            selectedCategory === category
+              ? "bg-zinc-900 text-white"
+              : "bg-zinc-200 text-gray-700"
+          } transition-all`}
+        >
+          {category}
+        </button>
+      ))}
+    </div>
+  );
 };
 
-export default CategoryFilter;
+export default Category;
