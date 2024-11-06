259dac6fed8736fbed6f03a57f308e65976d3018
adarsh02 <aadarshraazz007@gmail.com>
2024-11-07 01:29:22 +0530

    installed formik and yup via npm

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,5 +1,24 @@
 const Signup = (): JSX.Element => {
-  return <div>Signup</div>;
+  return (
+    <form>
+      <div>
+        <label htmlFor="">username</label>
+        <input type="text" placeholder="username" />
+      </div>
+      <div>
+        <label htmlFor="">Full Name</label>
+        <input type="text" placeholder="Enter your full name" />
+      </div>
+      <div>
+        <label htmlFor="">Email</label>
+        <input type="text" placeholder="Enter your email" />
+      </div>
+      <div>
+        <label htmlFor="">Password</label>
+        <input type="text" placeholder="Enter a password" />
+      </div>
+    </form>
+  );
 };
 
 export default Signup;
