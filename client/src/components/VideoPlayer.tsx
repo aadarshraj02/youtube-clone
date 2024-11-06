361e241ccd9359f0a8a4d9bc4734e89d04e3e8f9
adarsh02 <aadarshraazz007@gmail.com>
2024-11-06 22:15:24 +0530

    added layout for videPlayer

--- client/src/components/VideoPlayer.tsx
+++ client/src/components/VideoPlayer.tsx
@@ -1,37 +1,84 @@
+import { BiLike, BiDislike } from "react-icons/bi";
+import { FaRegBell } from "react-icons/fa";
+import { PiShareFatLight } from "react-icons/pi";
+import { LiaDownloadSolid } from "react-icons/lia";
+import { VscSend } from "react-icons/vsc";
+import { BsThreeDotsVertical } from "react-icons/bs";
+
 const VideoPlayer = (): JSX.Element => {
   return (
-    <div>
-      <div>
-        <img src="" alt="" />
+    <div className="w-full md:w-3/4 py-4 px-8">
+      <div className="w-full">
+        <img
+          className="w-full h-[40vh] sm:h-[50vh] md:h-[60vh] lg:h-[75vh] rounded-lg"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTABbXr4i-QODqhy7tofHYmTYh05rYPktzacw&s"
+          alt=""
+        />
       </div>
-      <div>
-        <h2>Title of video</h2>
+      <div className="my-1">
+        <h2 className="text-2xl uppercase font-medium">Title of video</h2>
       </div>
-      <div>
-        <h2>Channel Name</h2>
-        <button>Subscribe</button>
+      <div className="flex flex-col lg:flex-row lg:items-center justify-between gap-1">
+        <div className="flex gap-2 items-center my-1">
+          <img
+            className="h-8 w-8 rounded-full"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTABbXr4i-QODqhy7tofHYmTYh05rYPktzacw&s"
+            alt=""
+          />
+          <div className="flex flex-col ">
+            <h2 className="text-[14px]">Channel Name</h2>
+            <p className="text-[12px]">144k subscribers</p>
+          </div>
+          <button className="bg-black text-white px-2 py-1 rounded-full ml-6 hover:bg-zinc-500 transition-all ease-linear duration-300 flex items-center gap-1">
+            {" "}
+            <FaRegBell /> Subscribe{" "}
+          </button>
+        </div>
+        <div className="flex gap-3 ">
+          <button className="bg-zinc-900 text-white px-2 py-1 rounded-full flex items-center gap-1">
+            <BiLike />
+            100k
+          </button>
+          <button className="bg-zinc-900 text-white px-2 py-1 rounded-full flex items-center gap-1">
+            {" "}
+            <BiDislike /> 189k
+          </button>
+          <button className="bg-zinc-900 text-white px-2 py-1 rounded-full flex items-center gap-1">
+            <PiShareFatLight /> Share
+          </button>
+          <button className="bg-zinc-900 text-white px-2 py-1 rounded-full flex items-center gap-1">
+            <LiaDownloadSolid /> Download
+          </button>
+        </div>
       </div>
-      <div>
-        <button>Like</button>
-        <button>dislike</button>
-        <button>Share</button>
-        <button>Download</button>
+
+      <div className="flex gap-2 my-1">
+        <p className="text-sm">Views</p>
+        <p className="text-sm">Upload Time</p>
       </div>
-      <div>
-        <p>Views</p>
-        <p>Upload Time</p>
-      </div>
-      <div>
-        <p>Video Description</p>
-      </div>
-      <div>
+      <p className="text-zinc-700 my-3">Video Description</p>
+      <div className="my-5">
         <p>Total comments</p>
       </div>
-      <div>
-        <input type="text" placeholder="Add a comment" />
+      <div className="flex flex-col w-full">
+        <input
+          type="text"
+          placeholder="Add a comment"
+          className="border-b border-black px-1 outline-none "
+        />
+        <div className="flex gap-2 justify-end mt-2">
+          <button className="flex items-center justify-center gap-2">
+            <VscSend /> comment
+          </button>
+          <button className="flex items-center"> Cancel </button>
+        </div>
       </div>
-      <div>
-        <p>All comments by map</p>
+      <div className="my-2 flex items-center justify-between md:gap-4 gap-0">
+        <div>
+          <p className="text-[12px] ">Username</p>
+          <p className="text-sm">Comment is video is too bad ;)</p>
+        </div>
+        <BsThreeDotsVertical className="cursor-pointer" />
       </div>
     </div>
   );
--- client/src/pages/VideoPlayerPage.tsx
+++ client/src/pages/VideoPlayerPage.tsx
@@ -1,7 +1,27 @@
+import { useState } from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 import VideoPlayer from "../components/VideoPlayer";
 
 const VideoPlayerPage = (): JSX.Element => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prev : any) => !prev);
+  };
   return <div>
+    <div className="max-w-full overflow-hidden">
+        <Navbar onToggleSidebar={toggleSidebar} />
+        <div className="flex">
+          <div
+            className={`transition-all duration-300 ease-linear ${
+              isSidebarOpen ? "w-56" : "w-0"
+            }`}
+          >
+            <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+          </div>
+        </div>
+      </div>
     <VideoPlayer />
   </div>;
 };
