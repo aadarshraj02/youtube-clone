446ff049071211bde1c6be04508ff5ab2af43220
adarsh02 <aadarshraazz007@gmail.com>
2024-11-07 01:53:06 +0530

    added yup validation

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,4 +1,28 @@
+import * as Yup from "yup";
+
 const Signup = (): JSX.Element => {
+    
+  const validationSchema = Yup.object({
+    username: Yup.string()
+      .matches(
+        /^[a-z0-9]*$/,
+        "Username must contain only lowercase and numbers"
+      )
+      .required("Username is required"),
+    fullName: Yup.string().required("Full Name is required"),
+    email: Yup.string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must be of 6 characters")
+      .max(16, "Password must not exceed 16 characters")
+      .matches(
+        /^(?=.*[a-z0-9])(?=.*[!@#$%^&*])/,
+        "Password must contain a character a number or a special character"
+      )
+      .required("Password is required"),
+  });
+
   return (
     <form>
       <div>
