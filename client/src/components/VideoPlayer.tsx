850d996ae2f8cbd4f67aa129b4b0a8cba7c8dd28
adarsh02 <aadarshraazz007@gmail.com>
2024-11-06 21:07:29 +0530

    added basic structure for video page

--- client/src/components/VideoPlayer.tsx
+++ client/src/components/VideoPlayer.tsx
@@ -1,5 +1,40 @@
 const VideoPlayer = (): JSX.Element => {
-  return <div>VideoPlayer</div>;
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <h2>Title of video</h2>
+      </div>
+      <div>
+        <h2>Channel Name</h2>
+        <button>Subscribe</button>
+      </div>
+      <div>
+        <button>Like</button>
+        <button>dislike</button>
+        <button>Share</button>
+        <button>Download</button>
+      </div>
+      <div>
+        <p>Views</p>
+        <p>Upload Time</p>
+      </div>
+      <div>
+        <p>Video Description</p>
+      </div>
+      <div>
+        <p>Total comments</p>
+      </div>
+      <div>
+        <input type="text" placeholder="Add a comment" />
+      </div>
+      <div>
+        <p>All comments by map</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoPlayer;
