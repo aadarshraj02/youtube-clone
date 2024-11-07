f566c7b5016739a736200db472fba91876209b69
adarsh02 <aadarshraazz007@gmail.com>
2024-11-07 23:45:20 +0530

    added auth slice

---
+++ client/src/redux/slices/authSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  token: null,
+  isAuthenticated: false,
+  loading: false,
+  error: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    loginStart: (state) => {
+      (state.loading = true), (state.error = false);
+    },
+    loginSuccess: (state, action) => {
+      state.loading = false;
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isAuthenticated = true;
+    },
+    loginFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    logout: (state) => {
+      state.user = null;
+      state.token = null;
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+export const { loginStart, loginSuccess, loginFailure, logout } =
+  authSlice.actions;
+export default authSlice.reducer;
