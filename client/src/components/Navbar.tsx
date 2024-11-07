e1c871185ffdf79d010325bfa1f4b417c7fbb23e
adarsh02 <aadarshraazz007@gmail.com>
2024-11-08 02:37:45 +0530

    updated navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import { IoSearchOutline } from "react-icons/io5";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { CgProfile } from "react-icons/cg";
+
 const Navbar = ({
   onToggleSidebar,
 }: {
@@ -13,17 +15,17 @@ const Navbar = ({
           className="cursor-pointer"
           size={24}
         />
-        <img src="./youtubelogo.jpg" alt="YouTube Logo" className="h-10" />
+        <img src="./youtubelogo.jpg" alt="YouTube Logo" className="md:h-14 h-10" />
       </div>
       <div className="flex items-center gap-2">
         <input
           type="text"
           placeholder="Search videos here"
-          className="border rounded-full px-3 py-1 w-[25vw]"
+          className="border rounded-full px-3 py-1 sm:w-[25vw] w-[30vw]"
         />
         <IoSearchOutline className="text-gray-500" size={20} />
       </div>
-      <button className="text-sm font-semibold">Profile</button>
+      <button className="text-sm font-semibold flex items-center gap-1 border px-2 py-1 rounded-full "><CgProfile size={18} /> Profile</button>
     </nav>
   );
 };
