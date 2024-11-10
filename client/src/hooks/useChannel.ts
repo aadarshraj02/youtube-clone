434efdde090371cd6400fca7ed8ce6cad6715fe6
adarsh02 <aadarshraazz007@gmail.com>
2024-11-10 21:01:27 +0530

    added custom hook to get channel and create new channel

---
+++ client/src/hooks/useChannel.ts
@@ -0,0 +1,52 @@
+import axios from "axios";
+import {
+  setUserChannel,
+  setChannel,
+  setError,
+} from "../redux/slices/channelSlices";
+import { RootState } from "../redux/store";
+import { useDispatch, useSelector } from "react-redux";
+
+interface ChannelData {
+  channelName: string;
+  description: string;
+}
+
+export const useChannel = () => {
+  const dispatch = useDispatch();
+  const { userChannel, channel, error } = useSelector(
+    (state: RootState) => state.channel
+  );
+
+  const createChannel = async (channelData: ChannelData) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/channels",
+        channelData,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      dispatch(setUserChannel(response.data));
+    } catch (error: any) {
+      dispatch(
+        setError(error.response.data.message || "Failed to create channel")
+      );
+    }
+  };
+
+  const fetchChannel = async (channelId: string) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/api/channels/${channelId}`
+      );
+      dispatch(setChannel(response.data));
+    } catch (error: any) {
+      setError(error.response.data.message || "Failed to get channel");
+    }
+  };
+
+  return { createChannel, fetchChannel, userChannel, channel, error };
+};
