cff1d1d404acb3b345b1408953594b6a584e0a4f
adarsh02 <aadarshraazz007@gmail.com>
2024-11-12 22:50:57 +0530

    updated styling of videoUpload page

--- client/src/pages/VideoUpload.tsx
+++ client/src/pages/VideoUpload.tsx
@@ -48,8 +48,8 @@ const VideoUpload = (): JSX.Element => {
   };
 
   return (
-    <div className="p-5 mx-9 rounded-2xl shadow-md bg-white">
-      <h2 className="text-xl font-bold">Upload New Video</h2>
+    <div className="p-5 mx-9 my-5 rounded-2xl shadow-md bg-white">
+      <h2 className="text-xl sm:text-3xl  font-bold text-center">Upload New Video</h2>
       <Formik
         initialValues={{
           title: "",
@@ -64,23 +64,23 @@ const VideoUpload = (): JSX.Element => {
           <img
             src="https://dummyimage.com/600x400/d4d4d4/fff.jpg&text=No+Thumbnail"
             alt="thumbnail preview"
-            className="w-[350px] h-[25vh] rounded-lg object-cover"
+            className="w-[300px] h-[25vh] rounded-lg object-cover"
           />
           {uploadProgress > 0 && (
             <ProgressBar completed={uploadProgress} maxCompleted={100} />
           )}
-          <div>
-            <label htmlFor="thumbnailUrl" className="font-bold">
+          <div className="flex sm:items-center flex-col sm:flex-row gap-4">
+            <label htmlFor="thumbnailUrl" className="font-semibold">
               Thumbnail URL
             </label>
             <Field
               type="text"
               name="thumbnailUrl"
-              className="rounded-lg border outline-none px-3 py-1 mt-1"
+              className="rounded-lg border outline-none px-3 py-1 mt-1  sm:w-1/2 w-full "
             />
           </div>
-          <div>
-            <label htmlFor="video" className="font-bold">
+          <div className="flex sm:items-center flex-col sm:flex-row gap-4" >
+            <label htmlFor="video" className="font-semibold">
               Video File
             </label>
             <input
@@ -88,38 +88,38 @@ const VideoUpload = (): JSX.Element => {
               id="video"
               accept="video/*"
               onChange={handleVideoChange}
-              className="rounded-lg border outline-none px-3 py-1 mt-1"
+              className="rounded-lg border outline-none px-3 py-1 mt-1 sm:w-1/2 w-full"
               required
             />
           </div>
-          <div>
-            <label htmlFor="title" className="font-bold">
+          <div className="flex sm:items-center flex-col sm:flex-row gap-4">
+            <label htmlFor="title" className="font-semibold">
               Title
             </label>
             <Field
               type="text"
               name="title"
-              className="rounded-lg border outline-none px-3 py-1 mt-1"
+              className="rounded-lg border outline-none px-3 py-1 mt-1 sm:w-1/2 w-full"
             />
           </div>
-          <div>
-            <label htmlFor="description" className="font-bold">
+          <div className="flex sm:items-center flex-col sm:flex-row gap-4">
+            <label htmlFor="description" className="font-semibold">
               Description
             </label>
             <Field
               as="textarea"
               name="description"
-              className="rounded-lg border outline-none px-3 py-1 mt-1"
+              className="rounded-lg border outline-none px-3 py-1 mt-1 sm:w-1/2 w-full"
             />
           </div>
-          <div>
-            <label htmlFor="category" className="font-bold">
+          <div className="flex sm:items-center flex-col sm:flex-row gap-4">
+            <label htmlFor="category" className="font-semibold">
               Category
             </label>
             <Field
               as="select"
               name="category"
-              className="rounded-lg border outline-none px-3 py-1 mt-1"
+              className="rounded-lg border outline-none px-3 py-1 mt-1 sm:w-1/2 w-full"
             >
               <option value="">Select Category</option>
               {categories.map((cat) => (
@@ -129,12 +129,15 @@ const VideoUpload = (): JSX.Element => {
               ))}
             </Field>
           </div>
+          <div className="flex w-full items-center justify-center">
           <button
             type="submit"
-            className="py-2 px-4 bg-black text-white font-semibold mt-2 rounded-lg"
+            className="lg:w-1/4 sm:w-1/2 w-full py-2 px-4 bg-black text-white font-semibold mt-2 rounded-lg"
           >
             Upload Video
           </button>
+          </div>
+      
         </Form>
       </Formik>
     </div>
