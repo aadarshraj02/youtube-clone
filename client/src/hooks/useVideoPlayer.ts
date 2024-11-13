507ea88c6b9d8b8e27f0d510de22c6307eee3249
adarsh02 <aadarshraazz007@gmail.com>
2024-11-13 18:02:48 +0530

    added function to fetch video with comments

--- client/src/hooks/useVideoPlayer.ts
+++ client/src/hooks/useVideoPlayer.ts
@@ -1,43 +1,35 @@
-import { useEffect, useState } from "react";
 import axios from "axios";
-
-interface Video {
-  title: string;
-  description: string;
-  thumbnailUrl: string;
-  videoUrl: string;
-  views: number;
-  likes: number;
-  dislikes: number;
-  category: string;
-  comments: Comment[];
-}
-
-interface Comment {
-  commentText: string;
-  username: string;
-}
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../redux/store";
+import {
+  setVideo,
+  setVideoLoading,
+  setVideoError,
+} from "../redux/slices/videoPlayerSlice";
 
 export const useVideoPlayer = (videoId: string) => {
-  const [video, setVideo] = useState<Video | null>(null);
-  const [error, setError] = useState<string | null>(null);
+  const dispatch = useDispatch<AppDispatch>();
 
-  useEffect(() => {
-    const fetchVideo = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:5000/api/videos/${videoId}`
-        );
-        setVideo(response.data.video);
-      } catch (err: any) {
-        setError(err.response?.data?.message || "Failed to fetch video");
-      }
-    };
+  const { video, comments, isLoading, error } = useSelector(
+    (state: RootState) => state.videoPlayer
+  );
 
-    if (videoId) {
-      fetchVideo();
+  const fetchVideoDetails = async () => {
+    dispatch(setVideoLoading(true));
+    try {
+      const response = await axios.get(
+        `http://localhost:5000/api/videos/${videoId}`
+      );
+      const { video, comments } = response.data;
+      dispatch(setVideo({ video, comments }));
+    } catch (error: any) {
+      dispatch(
+        setVideoError(error.response?.data?.message || "Failed to fetch video")
+      );
+    } finally {
+      dispatch(setVideoLoading(false));
     }
-  }, [videoId]);
+  };
 
-  return { video, error };
+  return { video, comments, fetchVideoDetails, isLoading, error };
 };
